5b0dd88d660231f71f460f0b0e53dc5f8991e2a6
Muskan gupta <muskangupta15223@gmail.com>
2026-03-09 22:59:57 +0530

    [Muskan Gupta]: commited UC11-sort-contacts-by-name on AddressBookApp

--- src/main/java/com/addressbook/controller/AddressBookController.java
+++ src/main/java/com/addressbook/controller/AddressBookController.java
@@ -102,4 +102,9 @@ public class AddressBookController {
 
         return service.countContactsByState();
     }
+    @GetMapping("/{bookName}/sort/name")
+    public List<Contact> sortByName(@PathVariable String bookName) {
+
+        return service.sortContactsByName(bookName);
+    }
 }
